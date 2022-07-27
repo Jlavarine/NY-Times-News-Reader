4dae33267e456df75ffeb2c2fda260e8e7d5bb05
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-07-27 14:40:59 -0500

    Link the details page of an article to it's URI

--- src/Articles.js
+++ src/Articles.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './Articles.css'
 import { Link } from 'react-router-dom'
 const Articles = ({id, title, article}) => {
-    console.log(article.title)
+    const articleId = article.uri.split('/').pop()
     return (
         <div className='cards'>
             {article.multimedia ? <img className='article-image' alt={article.multimedia[0].caption}src={article.multimedia[0].url}/> : <img className='article-image' alt='clipart of newspaper' src='https://cdn-icons-png.flaticon.com/512/81/81460.png' />}
             <div className='card-title'>
                 <h2>{title}</h2>
                 <hr></hr>
-                <Link to={`/details/${title}`}><button>Show More</button></Link>
+                <Link to={`/details/${articleId}`}><button>Show More</button></Link>
             </div>
         </div>
     )
