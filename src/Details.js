7860f8dd6361486212317cc40df6ece8df3e44d2
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-07-27 15:06:56 -0500

    Update styling for details page

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Details.css'
 const Details = ({foundArticle}) => {
     return (
-        <div>
-            <h2>{foundArticle.title}</h2>
-            <p>{foundArticle.abstract}</p>
-            <p>{foundArticle.byline}</p>
-            <Link to='/'><button>Homepage</button></Link>
+        <div className="details-view">
+            <h2 className="details-title">{foundArticle.title}</h2>
+            <p className="details-abstract">{foundArticle.abstract}</p>
+            <p className="details-byline">{foundArticle.byline}</p>
+            <a className="details-link" style={{textDecoration: 'none', color: 'blue'}}href={foundArticle.url} target='_blank'>View on NY Times Website</a><br></br>
+            <Link to='/'><button className="details-homepage-button">Homepage</button></Link>
         </div>
     )
 }
